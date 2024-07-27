7344580c71d54e217f9ead25d530b386871c9b32
Lucky Jain <luckyjain7247@gmail.com>
2024-07-27 15:25:06 +0530

    unit tests basics with jest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,28 @@
-import { sum } from "."
+import { divide, mult, subtract, sum } from "."
 
-
-test('adds the numbers', () => {
-    expect(sum(10, 45)).toBe(55);
+beforeAll(() => { //setup
+    console.log("before each one");
 })
+
+describe('testing the calculative functions', () => { // test suite
+
+    // all the test cases
+    test('adds the numbers', () => { 
+        expect(sum(10, 45)).toBe(55); // assertion
+    })
+
+    test('subtracts the numbers', () => {
+        expect(subtract(10, 4)).toBe(6);
+    })
+
+    test('divides the numbers', () => {
+        expect(divide(10, 2)).toBe(5);
+    })
+    
+    test('multiplies the numbers', () => {
+        expect(mult(10, 45)).toBe(450);
+    })
+})
+
+
+afterAll(() => console.log("after each test")); // teardown
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export function sum(a: number, b: number): number {
     return a + b;
 }
+
+export function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+export function divide(a: number, b: number): number {
+    return a / b;
+}
+
+export function mult(a: number, b: number): number {
+    return a * b;
+}
