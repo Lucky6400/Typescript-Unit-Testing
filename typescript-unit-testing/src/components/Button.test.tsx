2e6553dbe403c8930e368672ec127be7b3023817
Lucky Jain <luckyjain7247@gmail.com>
2024-08-18 02:27:20 +0530

    deleted react app

--- typescript-unit-testing/src/components/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import Button from './Button'
-
-test('renders correctly and matches the snapshot', () => {
-   const { asFragment } = render(<Button label='MyButton' />);
-
-   expect(asFragment()).toMatchSnapshot('Button with "Click me" label');
-})
